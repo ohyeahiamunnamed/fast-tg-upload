0bcc7a10dccdc81be468a313b17ae630ecf8b933
Vipin Gupta <vipin0900@gmail.com>
2020-06-14 11:20:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Telegram Torrent Leecher 🔥🤖
+
+A Telegram Torrent (and youtube-dl) Leecher based on [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+## installing
+
+### The Easy Way
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+
+
+* `/ytdl`: This command should be used as reply to a [supported link](https://ytdl-org.github.io/youtube-dl/supportedsites.html)
+
+* `/leech`: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [this command will SPAM the chat and send the downloads a seperate files, if there is more than one file, in the specified torrent]
+
+* `/leech archive`: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [This command will create a .tar.gz file of the output directory, and send the files in the chat, splited into PARTS of 1024MiB each, due to Telegram limitations]
+
+
+## How to Use?
+
+* send any one of the available command, as a reply to a valid link.
+
+
+## Issues or Feature Requests
+
+* add issues / feature requests, [here](https://github.com/gupta-vipin/fast-tg-upload/issues/new).
+
+
+## Credits, and Thanks to
+
+* [Dan Tès](https://telegram.dog/haskell) for his [Pyrogram Library](https://github.com/pyrogram/pyrogram)
+* [Robots](https://telegram.dog/Robots) for their [@UploadBot](https://telegram.dog/UploadBot)
+* [@AjeeshNair](https://telegram.dog/AjeeshNait) for his [torrent.ajee.sh](https://torrent.ajee.sh)
+* [@gotstc](https://telegram.dog/gotstc), @aryanvikash, [@HasibulKabir](https://telegram.dog/HasibulKabir) for their TORRENT groups
+* [@ThankTelegram](https://telegram.dog/thanktelegram) fot PublicLeech group.
---
+++ tobrot/__main__.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+import os
+
+from tobrot import (
+    DOWNLOAD_LOCATION,
+    TG_BOT_TOKEN,
+    APP_ID,
+    API_HASH
+)
+
+from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
+
+from tobrot.plugins.new_join_fn import new_join_f, help_message_f
+from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f
+from tobrot.plugins.status_message_fn import (
+    status_message_f,
+    cancel_message_f,
+    upload_document_f
+)
+from tobrot.plugins.call_back_button_handler import button
+from tobrot.plugins.custom_thumbnail import (
+    save_thumb_nail,
+    clear_thumb_nail
+)
+
+
+if __name__ == "__main__" :
+    # create download directory, if not exist
+    if not os.path.isdir(DOWNLOAD_LOCATION):
+        os.makedirs(DOWNLOAD_LOCATION)
+    #
+    app = Client(
+        "LeechBot",
+        bot_token=TG_BOT_TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        workers=343
+    )
+    #
+    incoming_message_handler = MessageHandler(
+        incoming_message_f,
+        filters=Filters.command(["leech"])
+    )
+    app.add_handler(incoming_message_handler)
+    #
+    incoming_purge_message_handler = MessageHandler(
+        incoming_purge_message_f,
+        filters=Filters.command(["purge"])
+    )
+    app.add_handler(incoming_purge_message_handler)
+    #
+    incoming_youtube_dl_handler = MessageHandler(
+        incoming_youtube_dl_f,
+        filters=Filters.command(["ytdl"])
+    )
+    app.add_handler(incoming_youtube_dl_handler)
+    #
+    cancel_message_handler = MessageHandler(
+        cancel_message_f,
+        filters=Filters.command(["cancel"])
+    )
+    app.add_handler(cancel_message_handler)
+    #
+    upload_document_handler = MessageHandler(
+        upload_document_f,
+        filters=Filters.command(["upload"])
+    )
+    app.add_handler(upload_document_handler)
+    #
+    save_thumb_nail_handler = MessageHandler(
+        save_thumb_nail,
+        filters=Filters.command(["savethumbnail"])
+    )
+    app.add_handler(save_thumb_nail_handler)
+    #
+    clear_thumb_nail_handler = MessageHandler(
+        clear_thumb_nail,
+        filters=Filters.command(["clearthumbnail"])
+    )
+    app.add_handler(clear_thumb_nail_handler)
+    #
+    app.run()
